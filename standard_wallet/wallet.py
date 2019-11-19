00b5cdf8ac0b1908edf62d54edc4660528d3bbd4
Matthew Howard <matt@chia.net>
2019-11-19 12:06:26 +0000

    -added licenses to all code

--- standard_wallet/wallet.py
+++ standard_wallet/wallet.py
@@ -181,3 +181,17 @@ class Wallet:
         if transaction is None:
             return None  # TODO: Should we throw a proper error here, or just return None?
         return self.sign_transaction(transaction)
+
+
+"""
+Copyright 2018 Chia Network Inc
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
