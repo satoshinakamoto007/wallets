d1ccdbe49b9297e8cd9d39fa3c40717b4fd9580c
Yostra <straya@chia.net>
2019-11-20 09:27:59 -0800

    Revert "move to subdir"
    This reverts commit 42ec32180a515c5960d821ed90d6d62458da5f3d.

--- wallet/rl_wallet.py
+++ wallet/rl_wallet.py
@@ -1,22 +1,31 @@
 from chiasim.atoms import hexbytes
 
-from ..wallet.wallet import Wallet
+from .wallet import Wallet
 import hashlib
 import clvm
+import sys
 from chiasim.hashable import Program, ProgramHash, CoinSolution, SpendBundle, BLSSignature
 from binascii import hexlify
-
+from chiasim.validation.Conditions import (
+    conditions_by_opcode, make_create_coin_condition, make_assert_my_coin_id_condition, make_assert_min_time_condition
+)
 from chiasim.hashable.Coin import Coin
 from chiasim.hashable.CoinSolution import CoinSolutionList
 from clvm_tools import binutils
 from chiasim.wallet.BLSPrivateKey import BLSPrivateKey
 from chiasim.validation.Conditions import ConditionOpcode
 from chiasim.puzzles.p2_delegated_puzzle import puzzle_for_pk
+from chiasim.validation.consensus import (
+    conditions_for_solution, hash_key_pairs_for_conditions_dict
+)
 from chiasim.validation.Conditions import (
-    make_create_coin_condition, make_assert_my_coin_id_condition, make_assert_min_time_condition,
+    conditions_by_opcode, make_create_coin_condition, make_assert_my_coin_id_condition, make_assert_min_time_condition,
     make_assert_coin_consumed_condition
 )
 from chiasim.puzzles.p2_conditions import puzzle_for_conditions
+from .puzzle_utilities import pubkey_format, puzzlehash_from_string, BLSSignature_from_string
+from blspy import Signature
+from .keys import build_spend_bundle, sign_f_for_keychain
 import math
 
 
