b8c19107ed8b67f4bd142c1305797935e1a4cbc1
Yostra <straya@chia.net>
2019-12-02 14:46:07 -0800

    setup

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
         "authorised_payees",
         "atomic_swaps",
         "rate_limit",
-        "recoverable_wallet"
+        "recoverable_wallet",
+        "custody_wallet",
     ],
     license="Apache License",
     python_requires=">=3.7, <4",
@@ -24,7 +25,8 @@ setuptools.setup(
             "multisig_wallet = multisig.wallet:main",
             "signer = multisig.signer:main",
             "rl_wallet = rate_limit.rl_wallet_runnable:main",
-            "recoverable_wallet = recoverable_wallet.recoverable_wallet_runnable:main"
+            "recoverable_wallet = recoverable_wallet.recoverable_wallet_runnable:main",
+            "custody_wallet = custody_wallet.custody_wallet_runnable:main",
         ]
     },
     setup_requires=["clvm_tools", "setuptools_scm"],
