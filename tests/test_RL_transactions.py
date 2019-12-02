8511322bce060a9ad13b6bc55b2d204361b5af08
Yostra <straya@chia.net>
2019-12-02 14:54:16 -0800

    copyright stuff

--- tests/test_RL_transactions.py
+++ tests/test_RL_transactions.py
@@ -419,3 +419,17 @@ def test_rl_spend_all():
     assert wallet_a.current_balance == 999999700
     assert wallet_b.current_rl_balance == 0
     assert wallet_c.current_balance == 300
+
+
+"""
+Copyright 2018 Chia Network Inc
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
